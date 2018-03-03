541afffdf983b5468c3fdd485c813dd2c2e9464d
Mokuten OH <mokuteno@gmail.com>
2018-03-03 03:11:24 -0800

    integrate changes into entry file

--- react-backend/routes/index.js
+++ react-backend/routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var api_users = require('../api/api_users');
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/api_getuser', api_users.getUser);
+
 
 module.exports = router;
