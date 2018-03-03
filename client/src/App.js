a847f6791ca6c0ff3fc7aa88e1406f185d43dc5a
Mokuten OH <mokuteno@gmail.com>
2018-03-03 01:14:09 -0800

    remove unused resources

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 import RouterExample from './RouterExample';
 
 class App extends Component {
@@ -8,7 +6,6 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
