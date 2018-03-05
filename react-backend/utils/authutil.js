1bb11546d60bc6a608dbd370862c4e1fbfd4da5c
Mokuten OH <mokuteno@gmail.com>
2018-03-05 13:39:42 -0800

    add autiutil based on jwt token

---
+++ react-backend/utils/authutil.js
@@ -0,0 +1,25 @@
+var jwt = require('jsonwebtoken');
+var config = require('config');
+
+
+module.exports.verifyToken = function(token)
+{
+	var secret = config.jwt.secret;
+	jwt.verify(token, secret, function (err, decoded) {
+	    if (!err){
+	        return decoded;
+	    }
+	});
+	return null;
+}
+
+module.exports.generateToken = function (data, expire) {
+    var secret = config.jwt.secret;
+	var token = jwt.sign(
+		data,
+		secret,
+		{
+			expiresIn:  expire
+		});
+	return token;
+}
