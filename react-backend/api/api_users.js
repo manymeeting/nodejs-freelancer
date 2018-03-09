55dea32f6d109b7310672bb9b0b13064473fee9b
Mokuten OH <mokuteno@gmail.com>
2018-03-09 01:39:29 -0800

    generate jwt token when validation success

--- react-backend/api/api_users.js
+++ react-backend/api/api_users.js
@@ -1,4 +1,5 @@
 var dbUtil = require('../utils/dbutil');
+var authUtil = require('../utils/authUtil');
 
 module.exports.getUser = function (req, res, next) {
 	var connection = dbUtil.getDBConnection();
@@ -6,6 +7,7 @@ module.exports.getUser = function (req, res, next) {
 	var result = {};
 	connection.query('SELECT * from cmpefreelancer.users where id = ? ',[1] , function(err, rows, fields) {
 	  if (err) {
+	  	connection.end();
 	  	console.log(err);
 	  	throw err;	
 	  }
@@ -16,8 +18,6 @@ module.exports.getUser = function (req, res, next) {
 	  connection.end();
 	  return;
 	});
-
-	connection.end();
 }
 
 module.exports.validateUser = function (req, res, next) {
@@ -25,20 +25,58 @@ module.exports.validateUser = function (req, res, next) {
 	var password = req.body.password;
 
 	var connection = dbUtil.getDBConnection();
-	var result = {};
+	console.log(req.body);
 	connection.query('SELECT * FROM cmpefreelancer.users WHERE email = ? and password = ? ',[email, password] , function(err, rows, fields) {
 	  if (err) throw err;
-	  console.log('The result is: ', rows[0].name);
+
+	  if(!rows.length > 0)
+	  {
+	  	res.status(404).send("Invalid User");
+	  	connection.end();
+	  	return;
+	  }
+	  console.log('The validated user is: ', rows[0].name);
+
+	  var result = {};
 	  result.name = rows[0].name;
+	  result.id = rows[0].id;
+
+	  // validation passed, send jwt back to client
+	  var token = authUtil.generateToken({
+	  	user: {
+	  		id: result.id
+	  	}
+	  });
+
+	  result.token = token;
+
 	  res.type('json');
 	  res.send(JSON.stringify(result));
 	  connection.end();
 	  return;
 	});
-
-	connection.end();
 }
 
 
+module.exports.addUser = function (req, res, next) {
+	var userData = {
+		email: "",
+		password: "",
+		username: "",
+		skill: "",
+		avatarurl: "",
+		phone: "",
+		about: "",
+		skills: ""
+	};
+
+	userData = Object.assign(userData, req.body);
+
+	var connection = dbUtil.getDBConnection();
+	var result = {};
+	
+
+	connection.end();
+}
 
 
