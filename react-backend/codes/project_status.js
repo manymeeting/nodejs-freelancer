c790f7d05b4357dd1f03b0c01c59212a90d8365b
Mokuten OH <mokuteno@gmail.com>
2018-03-09 16:17:55 -0800

    add codes referring project status

---
+++ react-backend/codes/project_status.js
@@ -0,0 +1,7 @@
+module.exports.PROJECT_STATUS = {
+	OPEN: "OPEN", // Published by employer and open to new bids. No bid has been accepted yet.
+	STARTED: "STARTED", // Employer has selected a bid and the project has started.
+	FINISHED: "FINISHED", // Project has been finished.
+	CANCELED: "CANCELED", // Project has been canceled by the employer, or the project has expired without any accpeted bid.
+	DELAYED: "DELAYED", // project has been delayed.
+};
