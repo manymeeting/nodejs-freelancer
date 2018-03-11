be2e8ac0b2f8ff048e64773e1f2e5617863c153c
Mokuten OH <mokuteno@gmail.com>
2018-03-10 18:21:07 -0800

    add api functions for project and bid data

--- react-backend/api/api_projects.js
+++ react-backend/api/api_projects.js
@@ -4,6 +4,8 @@ var project_codes = require('../codes/project_codes');
 
 const TABLE_PROJCETS = ' ' + dbUtil.getDBName() + ".projects" + ' ';
 const TABLE_USERS = ' ' + dbUtil.getDBName() + ".users" + ' ';
+const TABLE_BIDS = ' ' + dbUtil.getDBName() + ".bids" + ' ';
+
 
 module.exports.getAllOpenProjects = function (req, res, next) {
 	var connection = dbUtil.getDBConnection();
@@ -26,3 +28,73 @@ module.exports.getAllOpenProjects = function (req, res, next) {
 	  return;
 	});
 }
+
+module.exports.getProjectDetails = function (req, res, next) {
+	var connection = dbUtil.getDBConnection();
+
+	var result = {};
+	var projectID = req.query.id;
+	console.log("projectID: " + projectID);
+	var queryStr = 
+		' select * from' + TABLE_PROJCETS + 
+		' left join' + TABLE_USERS + 'on projects.employer_id = users.id ' +
+		' where projects.id = ?';
+	
+	console.log(queryStr);
+	connection.query(queryStr,[projectID], function(err, rows, fields) {
+	  dbUtil.handleError(connection, err);
+
+	  result = rows[0];
+	  res.type('json');
+	  res.send(JSON.stringify(result));
+	  connection.end();
+	  return;
+	});
+}
+
+module.exports.getAllBidsOnProject = function (req, res, next) {
+	var connection = dbUtil.getDBConnection();
+
+	var result = {};
+	var projectID = req.query.id;
+	var queryStr = 
+		' select bids.id, bids.bidder_id, bids.period, bids.bid_date, bids.bid_price, users.name, users.avatarurl from' + TABLE_BIDS + 
+		' left join' + TABLE_USERS + 'on bids.bidder_id = users.id ' + 
+		' where bids.project_id = ?';
+	
+	console.log(queryStr);
+	connection.query(queryStr,[projectID], function(err, rows, fields) {
+	  dbUtil.handleError(connection, err);
+
+	  result = rows;
+	  res.type('json');
+	  res.send(JSON.stringify(result));
+	  connection.end();
+	  return;
+	});
+}
+
+module.exports.getAveBidPriceOnProject = function (req, res, next) {
+	var connection = dbUtil.getDBConnection();
+
+	var result = {};
+	var projectID = req.query.id;
+	var queryStr = 
+		' select avg(bids.bid_price) as avg_bid_price from' + TABLE_BIDS + 
+		' left join' + TABLE_PROJCETS + 'on bids.project_id = projects.id ' + 
+		' group by projects.id'
+		' where projects.id = ?';
+	
+	console.log(queryStr);
+	connection.query(queryStr,[projectID], function(err, rows, fields) {
+	  dbUtil.handleError(connection, err);
+
+	  result = rows[0];
+	  res.type('json');
+	  res.send(JSON.stringify(result));
+	  connection.end();
+	  return;
+	});
+}
+
+
--- react-backend/routes/router.js
+++ react-backend/routes/router.js
@@ -8,4 +8,7 @@ router.post('/api_auth_user', api_users.validateUser);
 router.put('/api_add_user', api_users.addUser);
 
 router.get('/api_get_all_open_proj', api_projects.getAllOpenProjects);
+router.get('/api_get_project_details', api_projects.getProjectDetails);
+router.get('/api_get_all_bids_on_proj', api_projects.getAllBidsOnProject);
+router.get('/api_get_ave_bid_price_on_proj', api_projects.getAveBidPriceOnProject);
 module.exports = router;
