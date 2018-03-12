b2df1fe1c958228b48eaffa32b8c99d8587d7b46
Mokuten OH <mokuteno@gmail.com>
2018-03-11 19:36:38 -0700

    enable Redux DevTools for Chrome

--- client/src/index.js
+++ client/src/index.js
@@ -5,13 +5,18 @@ import './css/bootstrap.css';
 import {
   BrowserRouter as Router
 } from 'react-router-dom';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 import reducer from './reducers/RootReducer';
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(reducer, applyMiddleware(thunk));
+const store = createStore(
+	reducer, 
+	composeEnhancers(
+		applyMiddleware(thunk)
+	));
 
 ReactDOM.render(
 	<Provider store={store}>
