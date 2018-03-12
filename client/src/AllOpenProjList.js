1bf53bd9d9c7132af01709d6ab296c0f988eb554
Mokuten OH <mokuteno@gmail.com>
2018-03-12 00:44:33 -0700

    render projects information in a list

--- client/src/AllOpenProjList.js
+++ client/src/AllOpenProjList.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 class AllOpenProjList extends React.Component {
 	constructor(props)
@@ -8,11 +9,39 @@ class AllOpenProjList extends React.Component {
 	}
 	render()
 	{
+		var allOpenProjects = this.props.allOpenProjects;
 		return (
 			// list of projects
-			<p>Here is AllOpenProjList!</p>
+			<div>
+				<ul>
+					{
+						allOpenProjects.map((project) =>
+							<li key={project.id}>
+								<div className="project-info-container">
+									<div>
+										<h3 className="project-title">{project.name}</h3>
+										<p className="project-desc">{project.description}</p>
+										<p className="project-skills">{project.skills}</p>
+									</div>
+									<div>
+										<p className="project-budget-range">{project.budget_range}</p>
+										<p className="project-employer-name">{project.name}</p>
+									</div>
+
+								</div>
+							</li>
+						)
+					}
+					
+				</ul>
+			</div>
 		);
 	}
 }
 
-export default AllOpenProjList;
+const mapStateToProps = state => ({
+  allOpenProjects: state.allOpenProjects.items,
+  error: state.allOpenProjects.error
+});
+
+export default connect(mapStateToProps)(AllOpenProjList);
