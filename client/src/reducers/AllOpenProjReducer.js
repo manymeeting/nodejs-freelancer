5dc9f09249e6e4345af4407db46f5368666a3950
Mokuten OH <mokuteno@gmail.com>
2018-03-11 19:22:38 -0700

    create reducers for fetching all open projects

---
+++ client/src/reducers/AllOpenProjReducer.js
@@ -0,0 +1,40 @@
+import {
+  FETCH_ALL_OPEN_PROJECTS_BEGIN,
+  FETCH_ALL_OPEN_PROJECTS_SUCCESS,
+  FETCH_ALL_OPEN_PROJECTS_FAILURE
+} from '../actions/AllOpenProjectActions';
+
+
+const initialState = {
+	items: [],
+	loading: false,
+	error: null
+};
+
+
+export default function AllOpenProjReducer(state = initialState, action) {
+  switch (action.type) {
+
+  case FETCH_ALL_OPEN_PROJECTS_BEGIN:
+  	return {
+  		...state,
+        loading: true,
+        error: null
+  	}
+  case FETCH_ALL_OPEN_PROJECTS_SUCCESS:
+  	return {
+  		...state,
+  		loading: false,
+  		items: state.items.concat([action.payload])
+  	};
+  case FETCH_ALL_OPEN_PROJECTS_FAILURE:
+  	return {
+        ...state,
+        loading: false,
+        error: action.payload,
+        items: []
+    };
+  default:
+    return state;
+  }
+}
