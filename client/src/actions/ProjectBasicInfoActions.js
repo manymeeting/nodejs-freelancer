eeacd6aec8531bc29d03abc8fbc6f19debc1b588
Mokuten OH <mokuteno@gmail.com>
2018-03-12 21:32:38 -0700

    create redux actions for project basic info

---
+++ client/src/actions/ProjectBasicInfoActions.js
@@ -0,0 +1,35 @@
+import ClientAuthService from '../utils/ClientAuthService';
+
+export const FETCH_PROJCET_BASIC_INFO_BEGIN   = 'FETCH_PROJCET_BASIC_INFO_BEGIN';
+export const FETCH_PROJCET_BASIC_INFO_SUCCESS = 'FETCH_PROJCET_BASIC_INFO_SUCCESS';
+export const FETCH_PROJCET_BASIC_INFO_FAILURE = 'FETCH_PROJCET_BASIC_INFO_FAILURE';
+
+export const fetchProjBasicInfoBegin = () => ({
+  type: FETCH_PROJCET_BASIC_INFO_BEGIN
+});
+
+export const fetchProjBasicInfoSuccess = projectDetails => ({
+  type: FETCH_PROJCET_BASIC_INFO_SUCCESS,
+  payload: projectDetails
+});
+
+export const fetchProjBasicInfoError = error => ({
+  type: FETCH_PROJCET_BASIC_INFO_FAILURE,
+  payload: error
+});
+
+
+export function fetchProjBasicInfo(id) {
+	
+	var clientAuthService = new ClientAuthService();
+
+	return dispatch => {
+		dispatch(fetchProjBasicInfoBegin());
+		return clientAuthService.fetch('/api_get_project_details?id=' + id, {method: "GET"})
+			.then(data => {
+		        dispatch(fetchProjBasicInfoSuccess(data));
+		        return true;
+      		})
+      		.catch(error => dispatch(fetchProjBasicInfoError(error)));
+	}
+}
