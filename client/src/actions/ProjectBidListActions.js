da4acece54e6d15c0f2a2f65ff7c4bed5191b18f
Mokuten OH <mokuteno@gmail.com>
2018-03-12 22:59:45 -0700

    add actions for project bid list

---
+++ client/src/actions/ProjectBidListActions.js
@@ -0,0 +1,35 @@
+import ClientAuthService from '../utils/ClientAuthService';
+
+export const FETCH_PROJCET_BID_LIST_BEGIN   = 'FETCH_PROJCET_BID_LIST_BEGIN';
+export const FETCH_PROJCET_BID_LIST_SUCCESS = 'FETCH_PROJCET_BID_LIST_SUCCESS';
+export const FETCH_PROJCET_BID_LIST_FAILURE = 'FETCH_PROJCET_BID_LIST_FAILURE';
+
+export const fetchProjBidListBegin = () => ({
+  type: FETCH_PROJCET_BID_LIST_BEGIN
+});
+
+export const fetchProjBidListSuccess = projectDetails => ({
+  type: FETCH_PROJCET_BID_LIST_SUCCESS,
+  payload: projectDetails
+});
+
+export const fetchProjBidListError = error => ({
+  type: FETCH_PROJCET_BID_LIST_FAILURE,
+  payload: error
+});
+
+
+export function fetchProjBidList(id) {
+	
+	var clientAuthService = new ClientAuthService();
+
+	return dispatch => {
+		dispatch(fetchProjBidListBegin());
+		return clientAuthService.fetch('/api_get_all_bids_on_proj?id=' + id, {method: "GET"})
+			.then(data => {
+		        dispatch(fetchProjBidListSuccess(data));
+		        return true;
+      		})
+      		.catch(error => dispatch(fetchProjBidListError(error)));
+	}
+}
