c2c897d02f5a93e074fb8bcd3ac79047fdf5f241
Mokuten OH <mokuteno@gmail.com>
2018-03-15 12:40:28 -0700

    add reducer and action to add bid

---
+++ client/src/actions/BidOnProjectActions.js
@@ -0,0 +1,48 @@
+import ClientAuthService from '../utils/ClientAuthService';
+
+export const ADD_BID_ON_PROJECT_BEGIN   = 'ADD_BID_ON_PROJECT_BEGIN';
+export const ADD_BID_ON_PROJECT_SUCCESS = 'ADD_BID_ON_PROJECT_SUCCESS';
+export const ADD_BID_ON_PROJECT_FAILURE = 'ADD_BID_ON_PROJECT_FAILURE';
+
+export const addBidOnProjectBegin = () => ({
+  type: ADD_BID_ON_PROJECT_BEGIN
+});
+
+export const addBidOnProjectSuccess = data => ({
+  type: ADD_BID_ON_PROJECT_SUCCESS,
+  payload: data
+});
+
+export const addBidOnProjectError = error => ({
+  type: ADD_BID_ON_PROJECT_FAILURE,
+  payload: error
+});
+
+
+export function addBidOnProject(params) {
+	
+	var clientAuthService = new ClientAuthService();
+	var newBid = {
+		projectID: params.projectID,
+		bidderID: params.bidderID,
+		employerID: params.employerID,
+		bidPeriod: params.bidPeriod,
+		bidDate: params.bidDate,
+		bidPrice: params.bidPrice
+	};
+	return dispatch => {
+		dispatch(addBidOnProjectBegin());
+		return clientAuthService.fetch('/api_add_bid_on_proj', {
+		        method: 'PUT',
+		        body: JSON.stringify(newBid)
+	    	})
+			.then(data => {
+		        dispatch(addBidOnProjectSuccess({
+		        	project_id: params.projectID,
+		        	bid_id: data.insertId
+		        }));
+		        return true;
+      		})
+      		.catch(error => dispatch(addBidOnProjectError(error)));
+	}
+}
--- client/src/reducers/BiddedProjReducer.js
+++ client/src/reducers/BiddedProjReducer.js
@@ -1,7 +1,36 @@
-export default function BiddedProjReducer(state = [], action) {
+import {
+  ADD_BID_ON_PROJECT_BEGIN,
+  ADD_BID_ON_PROJECT_SUCCESS,
+  ADD_BID_ON_PROJECT_FAILURE
+} from '../actions/BidOnProjectActions';
+
+
+const initialState = {
+  bids: [],
+	loading: false,
+	error: null
+};
+
+export default function BiddedProjReducer(state = initialState, action) {
   switch (action.type) {
-  case 'ADD_PUBLISHED_PROJECT':
-    return state.concat([action.newProject]);
+  case ADD_BID_ON_PROJECT_BEGIN:
+  	return {
+  		...state,
+        loading: true,
+        error: null
+  	}
+  case ADD_BID_ON_PROJECT_SUCCESS:
+  	return {
+  		...state,
+  		loading: false,
+  		bids: state.bids.concat([action.payload])
+  	};
+  case ADD_BID_ON_PROJECT_FAILURE:
+  	return {
+        ...state,
+        loading: false,
+        error: action.payload
+    };
   default:
     return state;
   }
