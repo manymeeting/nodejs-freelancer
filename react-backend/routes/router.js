93485da145b642054ebaf5d09729b977206cb665
Mokuten OH <mokuteno@gmail.com>
2018-03-16 00:57:08 -0700

    add route to fetch basic user data

--- react-backend/routes/router.js
+++ react-backend/routes/router.js
@@ -4,6 +4,7 @@ var api_users = require('../api/api_users');
 var api_projects = require('../api/api_projects');
 var api_bids = require('../api/api_bids');
 
+router.post('/api_get_user', api_users.getUser);
 router.post('/api_auth_user', api_users.validateUser);
 router.put('/api_add_user', api_users.addUser);
 
