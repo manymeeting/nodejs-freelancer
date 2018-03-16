6bad6338bae20b88e93bbcd1f1341d5741f7e958
Mokuten OH <mokuteno@gmail.com>
2018-03-15 20:18:08 -0700

    add action and reducer for all published projects

---
+++ client/src/actions/AllPublishedProjectAction.js
@@ -0,0 +1,35 @@
+import ClientAuthService from '../utils/ClientAuthService';
+
+export const FETCH_ALL_PUBLISHED_PROJECT_BEGIN   = 'FETCH_ALL_PUBLISHED_PROJECT_BEGIN';
+export const FETCH_ALL_PUBLISHED_PROJECT_SUCCESS = 'FETCH_ALL_PUBLISHED_PROJECT_SUCCESS';
+export const FETCH_ALL_PUBLISHED_PROJECT_FAILURE = 'FETCH_ALL_PUBLISHED_PROJECT_FAILURE';
+
+export const fetchAllPublishedProjectBegin = () => ({
+  type: FETCH_ALL_PUBLISHED_PROJECT_BEGIN
+});
+
+export const fetchAllPublishedProjectSuccess = projects => ({
+  type: FETCH_ALL_PUBLISHED_PROJECT_SUCCESS,
+  payload: projects
+});
+
+export const fetchAllPublishedProjectError = error => ({
+  type: FETCH_ALL_PUBLISHED_PROJECT_FAILURE,
+  payload: error
+});
+
+
+export function fetchAllPublishedProject(id) {
+	
+	var clientAuthService = new ClientAuthService();
+
+	return dispatch => {
+		dispatch(fetchAllPublishedProjectBegin());
+		return clientAuthService.fetch('/api_get_all_proj_published_by_user?id=' + id, {method: "GET"})
+			.then(data => {
+		        dispatch(fetchAllPublishedProjectSuccess(data));
+		        return true;
+      		})
+      		.catch(error => dispatch(fetchAllPublishedProjectError(error)));
+	}
+}
---
+++ client/src/reducers/AllPublishedProjReducer.js
@@ -0,0 +1,40 @@
+import {
+  FETCH_ALL_PUBLISHED_PROJECT_BEGIN,
+  FETCH_ALL_PUBLISHED_PROJECT_SUCCESS,
+  FETCH_ALL_PUBLISHED_PROJECT_FAILURE
+} from '../actions/AllPublishedProjectAction';
+
+
+const initialState = {
+	items: [],
+	loading: false,
+	error: null
+};
+
+
+export default function AllOpenProjReducer(state = initialState, action) {
+  switch (action.type) {
+
+  case FETCH_ALL_PUBLISHED_PROJECT_BEGIN:
+  	return {
+  		...state,
+        loading: true,
+        error: null
+  	}
+  case FETCH_ALL_PUBLISHED_PROJECT_SUCCESS:
+  	return {
+  		...state,
+  		loading: false,
+  		items: action.payload
+  	};
+  case FETCH_ALL_PUBLISHED_PROJECT_FAILURE:
+  	return {
+        ...state,
+        loading: false,
+        error: action.payload,
+        items: []
+    };
+  default:
+    return state;
+  }
+}
