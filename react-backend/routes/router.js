ca70b284a19c981d9d37eec4dd46f190897d8f66
Mokuten OH <mokuteno@gmail.com>
2018-03-16 16:26:48 -0700

    add router for post project api

--- react-backend/routes/router.js
+++ react-backend/routes/router.js
@@ -12,10 +12,10 @@ router.get('/api_get_all_open_proj', api_projects.getAllOpenProjects);
 router.get('/api_get_project_details', api_projects.getProjectDetails);
 router.get('/api_get_all_proj_bidded_by_user', api_projects.getAllProjBiddedByUser);
 router.get('/api_get_all_proj_published_by_user', api_projects.getAllProjPublishedByUser);
+router.put('/api_add_project', api_projects.postProject);
 
 router.get('/api_get_all_bids_on_proj', api_bids.getAllBidsOnProject);
 router.get('/api_get_ave_bid_price_on_proj', api_bids.getAveBidPriceOnProject);
-
 router.put('/api_add_bid_on_proj', api_bids.addBidOnProject);
 
 module.exports = router;
