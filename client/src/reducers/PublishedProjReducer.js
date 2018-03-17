a6fc09ecde082534921e13bd9ab80d00a2632ba7
Mokuten OH <mokuteno@gmail.com>
2018-03-16 22:59:54 -0700

    remove unused reducer

--- client/src/reducers/PublishedProjReducer.js
+++
@@ -1,8 +0,0 @@
-export default function PublishedProjReducer(state = [], action) {
-  switch (action.type) {
-  case 'ADD_BIDDED_PROJECT':
-    return state.concat([action.newProject]);
-  default:
-    return state;
-  }
-}
