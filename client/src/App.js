d45b0b89deb7483b74d2353af2da516a60792cf4
Mokuten OH <mokuteno@gmail.com>
2018-03-17 03:18:50 -0700

    integrate update user profile feature

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,7 @@ import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Home from './pages/Home';
 import Dashboard from './pages/Dashboard';
+import UserProfile from './pages/UserProfile';
 import PostProject from './pages/PostProject';
 import ProjectDetails from './pages/ProjectDetails';
 import ClientAuthService from './utils/ClientAuthService';
@@ -31,6 +32,7 @@ class App extends Component {
           <Route path='/dashboard' component={Dashboard}/>
           <Route path='/post_project' component={PostProject}/>
           <Route path='/project_details' component={ProjectDetails}/>
+          <Route path='/user_profile' component={UserProfile}/>
         </Switch>
       </div>
     );
