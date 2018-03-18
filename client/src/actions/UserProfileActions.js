decc799a299af42a6bba866966a1db02a25d5348
Mokuten OH <mokuteno@gmail.com>
2018-03-17 17:56:09 -0700

    add redux actions to update user avatar

--- client/src/actions/UserProfileActions.js
+++ client/src/actions/UserProfileActions.js
@@ -8,7 +8,12 @@ export const UPDATE_USER_PROFILE_BEGIN   = 'UPDATE_USER_PROFILE_BEGIN';
 export const UPDATE_USER_PROFILE_SUCCESS = 'UPDATE_USER_PROFILE_SUCCESS';
 export const UPDATE_USER_PROFILE_FAILURE = 'UPDATE_USER_PROFILE_FAILURE';
 
+export const UPDATE_USER_AVATAR_BEGIN   = 'UPDATE_USER_AVATAR_BEGIN';
+export const UPDATE_USER_AVATAR_SUCCESS = 'UPDATE_USER_AVATAR_SUCCESS';
+export const UPDATE_USER_AVATAR_FAILURE = 'UPDATE_USER_AVATAR_FAILURE';
 
+
+// fetch profile
 export const fetchUserProfileBegin = () => ({
   type: FETCH_USER_PROFILE_BEGIN
 });
@@ -24,6 +29,8 @@ export const fetchUserProfileError = error => ({
   payload: error
 });
 
+
+// update profile
 export const updateUserProfileBegin = () => ({
   type: UPDATE_USER_PROFILE_BEGIN
 });
@@ -36,6 +43,23 @@ export const updateUserProfileSuccess = data => ({
 
 export const updateUserProfileError = error => ({
   type: UPDATE_USER_PROFILE_FAILURE,
+  payload: error
+});
+
+
+// update avatar
+export const updateUserAvatarBegin = () => ({
+  type: UPDATE_USER_AVATAR_BEGIN
+});
+
+export const updateUserAvatarSuccess = data => ({
+  type: UPDATE_USER_AVATAR_SUCCESS,
+  payload: data
+
+});
+
+export const updateUserAvatarError = error => ({
+  type: UPDATE_USER_AVATAR_FAILURE,
   payload: error
 });
 
@@ -69,17 +93,38 @@ export function updateUserProfile(id, params) {
       userSkills: params.userSkills
     };
   return dispatch => {
-    dispatch(fetchUserProfileBegin());
+    dispatch(updateUserProfileBegin());
     return clientAuthService
           .fetch('/api_update_user?id=' + id, {
               method: "POST",
               body: JSON.stringify(newProfile)})
           .then(data => {
-            dispatch(fetchUserProfileSuccess(data));
+            dispatch(updateUserProfileSuccess(data));
             return true;
           })
           .catch(error => {
-            dispatch(fetchUserProfileError(error));
+            dispatch(updateUserProfileError(error));
+            throw error;  
+          });
+  }
+}
+
+
+export function updateUserAvatar(formData) {
+  var clientAuthService = new ClientAuthService();
+  
+  return dispatch => {
+    dispatch(updateUserAvatarBegin());
+    return clientAuthService
+          .fetch('/api_update_avatar', {
+              method: "POST",
+              body: formData}, true)
+          .then(data => {
+            dispatch(updateUserAvatarSuccess(data));
+            return true;
+          })
+          .catch(error => {
+            dispatch(updateUserAvatarError(error));
             throw error;  
           });
   }
