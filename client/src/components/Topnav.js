1fb11cf342471b4cd292948da05acc22bbdf4585
Mokuten OH <mokuteno@gmail.com>
2018-03-18 14:11:56 -0700

    update topnav styles

--- client/src/components/Topnav.js
+++ client/src/components/Topnav.js
@@ -31,22 +31,22 @@ class TopNav extends React.Component {
   render()
   {
     return (
-      <nav className="fl-top-nav navbar navbar-expand-lg navbar-light bg-light">
+      <nav className="fl-top-nav navbar navbar-expand-lg navbar-light">
         <div className="navbar-brand"><img alt="logo" className="fl-top-nav-logo" src={logo} /></div>
 
         <div className="fl-top-nav-links collapse navbar-collapse">
           <ul className="navbar-nav mr-auto">
-            <li className="nav-item"><Link to='/home'>Home</Link></li>
-            <li className="nav-item"><Link to='/signup'>Sign Up</Link></li>
-            <li className="nav-item"><Link to='/dashboard/published'>Dashboard</Link></li>
-            <li className="nav-item"><Link to='/post_project'>Post Project</Link></li>
+            <li className="nav-item fl-top-nav-item"><Link to='/home'>Home</Link></li>
+            <li className="nav-item fl-top-nav-item"><Link to='/dashboard/published'>Dashboard</Link></li>
+            <li className="nav-item fl-top-nav-item"><Link to='/post_project'>Post Project</Link></li>
+            <li className="nav-item fl-top-nav-item"><Link to='/signup'>New Account</Link></li>
           </ul>
         </div>
 
         <div className="fl-top-user-info">
-          <span className="fl-top-user-avatar"><img  alt="avatar" src={this.props.userInfo.user_avatarurl ? this.props.userInfo.user_avatarurl : defaultAvatar} /></span>
+          <span className="fl-top-user-avatar"><img alt="avatar" src={this.props.userInfo.user_avatarurl ? this.props.userInfo.user_avatarurl : defaultAvatar} /></span>
           <span className="fl-top-user-name"><Link to={`/user_profile/${this.props.userInfo.user_id}`}>{this.props.userInfo.user_name}</Link></span>
-          <button id="signOutBtn" onClick={this.onSignOut}>Log out</button>
+          <button className="btn btn-danger" id="signOutBtn" onClick={this.onSignOut}>Log out</button>
         </div>
       </nav>
     );
