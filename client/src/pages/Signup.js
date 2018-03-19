49c508069a8fb068ce237dffd38f16e5c9e846d8
Mokuten OH <mokuteno@gmail.com>
2018-03-18 17:06:12 -0700

    update styles on sign up page

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -30,19 +30,25 @@ class SignupForm extends React.Component {
   render()
   {
     return (
-      <div id="signUpForm">
-        <h3>Form</h3>
-        <div className="container">
-          <form>
-            <span>Name</span><input type="text" name="username" id="inputName" onChange={this.handleInputChange} /><br/>
-            <span>Email</span><input type="email" name="email" id="inputEmail" onChange={this.handleInputChange} /><br/>
-            <span>Password</span><input type="password" name="password" id="inputPassword" onChange={this.handleInputChange} /><br/>
-          </form>
-          
-          <button id="signUpSubmitBtn" onClick={this.onSumbit}>
+      <div id="signUpForm" className="fl-main-container">
+        <h1 className="fl-main-header">Create New Account</h1>
+        <form>
+          <div className="form-group">
+              <label>User Name:</label>
+              <input type="text" className="form-control" name="username" onChange={this.handleInputChange} required/>
+          </div>
+          <div className="form-group">
+              <label>Email Address:</label>
+              <input type="email" className="form-control" name="email" onChange={this.handleInputChange} required/>
+          </div>
+          <div className="form-group">
+              <label>Password:</label>
+              <input type="password" className="form-control" name="password" onChange={this.handleInputChange} required/>
+          </div>
+          <button className="btn btn-primary" id="signUpSubmitBtn" onClick={this.onSumbit}>
             Sign Up
           </button>
-        </div>
+        </form>
       </div>
     );
   }
@@ -77,9 +83,6 @@ SignupForm = withRouter(SignupForm);
 
 
 const Signup = () => (
-  <div>
-    <p>Signup</p>
-    <SignupForm />
-  </div>
+  <SignupForm />
 )
 export default Signup;
