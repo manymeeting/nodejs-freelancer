b392c78838b9469cf16c837bea89802337165989
Mokuten OH <mokuteno@gmail.com>
2018-04-07 01:04:46 -0700

    create kafka-client and kafka-backend servers

---
+++ kafka-backend/app.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+module.exports = app;
