a2d588e3a0f86c8de4abb90c4aee51130d39f64b
Mokuten OH <mokuteno@gmail.com>
2018-04-14 17:55:37 -0700

    update mapping kafka client router

--- kafka-client/routes/router.js
+++ kafka-client/routes/router.js
@@ -7,19 +7,19 @@ router.get('/users/:id/profile', getMessenger.sendGET);
 router.post('/users', getMessenger.sendGET);
 router.post('/users/validation', getMessenger.sendGET);
 
-// router.get('/projects', api_m_projects.searchProjects);
-// router.get('/projects/:id', api_m_projects.getProjectDetails);
-// router.get('/projects/status/:status', api_m_projects.getAllProjectsOnStatus);
-// router.get('/projects/bidder/:id', api_m_projects.getAllProjBiddedByUser);
-// router.get('/projects/publisher/:id', api_m_projects.getAllProjPublishedByUser);
+router.get('/projects', getMessenger.sendGET);
+router.get('/projects/:id', getMessenger.sendGET);
+router.get('/projects/status/:status', getMessenger.sendGET);
+router.get('/projects/bidder/:id', getMessenger.sendGET);
+router.get('/projects/publisher/:id', getMessenger.sendGET);
 // router.post('/projects', api_m_projects.postProject);
 // router.post('/projects/notification/hire', api_emails.sendBidHiredEmail);
 // router.put('/projects/:id/status/:status', api_m_projects.updateStatus);
 // router.put('/projects/:projectID/hire/:bidID', api_m_projects.hireBid);
 
-// router.get('/transactions/users/:id/income', api_m_transactions.getIncomeTransOnUser);
-// router.get('/transactions/users/:id/expense', api_m_transactions.getExpenseTransOnUser);
-// router.get('/transactions/users/:id', api_m_transactions.getTransactionsOnUser);
+router.get('/transactions/users/:id/income', getMessenger.sendGET);
+router.get('/transactions/users/:id/expense', getMessenger.sendGET);
+router.get('/transactions/users/:id', getMessenger.sendGET);
 // router.post('/transactions', api_m_transactions.createTransaction);
 
 module.exports = router;
