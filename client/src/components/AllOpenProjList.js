4287cd53c75afbedbef259bbd1450750eb16aa7d
Mokuten OH <mokuteno@gmail.com>
2018-04-15 12:39:21 -0700

    update old api in all open project list

--- client/src/components/AllOpenProjList.js
+++ client/src/components/AllOpenProjList.js
@@ -23,30 +23,35 @@ class AllOpenProjList extends React.Component {
 			<div className="fl-all-open-proj-container">
 				<ul>
 					{
-						allOpenProjects.map((project) =>
-							<li key={project._id}>
-								<div className="fl-project-info-container">
-									<p className="fl-project-title"><Link to={"/project_details/" + project._id} >{project.project_name}</Link></p>
-									<div className="fl-list-row">
-										<span className="fl-list-label">Description: </span>
-										<span className="fl-project-desc">{project.project_description}</span>
-									</div>
-									<div className="fl-list-row">
-										<span className="fl-list-label">Required Skills: </span>
-										<span className="fl-project-skills">{project.project_skills}</span>
-									</div>
-									<div className="fl-list-row">
-										<span className="fl-list-label">Budget Range: </span>
-										<span className="fl-project-budget-range">{project.project_budget_range}</span> 
-										<span className="fl-list-label fl-margin-l-5">Published By: </span>
-										<span className="fl-project-employer-name"><Link to={"/users/" + project.employer_id + '/profile'} >{project.user_name}</Link></span>
-									</div>
-									<div className="fl-list-row">
-										<Link to={"/project_details/" + project._id} >Bid Now</Link>
-									</div>
+						allOpenProjects.map((project) => {
+							var employer = project.employer ? project.employer : {};
+							return(
+								<li key={project._id}>
+									<div className="fl-project-info-container">
+										<p className="fl-project-title"><Link to={"/project_details/" + project._id} >{project.project_name}</Link></p>
+										<div className="fl-list-row">
+											<span className="fl-list-label">Description: </span>
+											<span className="fl-project-desc">{project.project_description}</span>
+										</div>
+										<div className="fl-list-row">
+											<span className="fl-list-label">Required Skills: </span>
+											<span className="fl-project-skills">{project.project_skills}</span>
+										</div>
+										<div className="fl-list-row">
+											<span className="fl-list-label">Budget Range: </span>
+											<span className="fl-project-budget-range">{project.project_budget_range}</span> 
+											<span className="fl-list-label fl-margin-l-5">Published By: </span>
+											<span className="fl-project-employer-name"><Link to={"/user_profile/" + employer._id} >{employer.user_name}</Link></span>
+										</div>
+										<div className="fl-list-row">
+											<Link to={"/project_details/" + project._id} >Bid Now</Link>
+										</div>
 
-								</div>
-							</li>
+									</div>
+								</li>
+							)
+						}
+							
 						)
 					}
 					
