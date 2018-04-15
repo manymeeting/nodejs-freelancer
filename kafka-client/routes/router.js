d5be427ffa0c9382c446dc99359eff73e04d7581
Mokuten OH <mokuteno@gmail.com>
2018-04-15 14:23:09 -0700

    add customized middleware to handle various file uplaod

--- kafka-client/routes/router.js
+++ kafka-client/routes/router.js
@@ -1,15 +1,30 @@
 var express = require('express');
+var path = require('path');
 var router = express.Router();
+
 var getMessenger = require('../messengers/GETMessenger');
 var postMessenger = require('../messengers/POSTMessenger');
 var putMessenger = require('../messengers/PUTMessenger');
 
+var multer = require('multer');
+var avatarUpload = multer({ dest: path.join(__dirname, '../public/avatars/') })
 
 router.get('/users/:id', getMessenger.sendGET);
 router.get('/users/:id/profile', getMessenger.sendGET);
 router.post('/users', postMessenger.sendPOST);
 router.post('/users/validation', postMessenger.sendPOST);
 router.put('/users/:id', putMessenger.sendPUT);
+router.put('/users/:id/avatar', 
+	avatarUpload.single('file'), 
+	putMessenger.sendPUTWithFiles(function(req){
+		console.log("here ~~~~~~~" + req.file);
+		var fileData = {};
+		if (req.file) {
+			console.dir(req.file);
+			fileData.userAvatarURL = "/avatars/" + req.file.filename;
+		}
+		return fileData;
+}));
 
 router.get('/projects', getMessenger.sendGET);
 router.get('/projects/:id', getMessenger.sendGET);
--- services/routes/router.js
+++ services/routes/router.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var path = require('path');
 var router = express.Router();
-var api_users = require('../api/api_users');
-var api_projects = require('../api/api_projects');
-var api_bids = require('../api/api_bids');
+// var api_users = require('../api/api_users');
+// var api_projects = require('../api/api_projects');
+// var api_bids = require('../api/api_bids');
 
 var api_m_users = require('../api_mongo/api_users_mongo');
 var api_m_projects = require('../api_mongo/api_projects_mongo');
@@ -11,27 +11,27 @@ var api_m_transactions = require('../api_mongo/api_transactions_mongo');
 var api_emails = require('../api_mongo/api_emails');
 
 
-var multer = require('multer');
-var upload = multer({ dest: path.join(__dirname, '../public/avatars/') })
+// var multer = require('multer');
+// var upload = multer({ dest: path.join(__dirname, '../public/avatars/') })
 
 // Services on MySQL
-router.get('/api_get_user', api_users.getUser);
-router.get('/api_get_profile', api_users.getProfile);
-router.post('/api_auth_user', api_users.validateUser);
-router.post('/api_update_user', api_users.updateUser);
-router.put('/api_add_user', api_users.addUser);
-router.post('/api_update_avatar', upload.single('file'), api_users.updateAvatar);
+// router.get('/api_get_user', api_users.getUser);
+// router.get('/api_get_profile', api_users.getProfile);
+// router.post('/api_auth_user', api_users.validateUser);
+// router.post('/api_update_user', api_users.updateUser);
+// router.put('/api_add_user', api_users.addUser);
+// router.post('/api_update_avatar', upload.single('file'), api_users.updateAvatar);
 
-router.get('/api_get_all_open_proj', api_projects.getAllOpenProjects);
-router.get('/api_get_project_details', api_projects.getProjectDetails);
-router.get('/api_get_all_proj_bidded_by_user', api_projects.getAllProjBiddedByUser);
-router.get('/api_get_all_proj_published_by_user', api_projects.getAllProjPublishedByUser);
-router.put('/api_add_project', api_projects.postProject);
-router.post('/api_hire_bid', api_projects.hireBid);
+// router.get('/api_get_all_open_proj', api_projects.getAllOpenProjects);
+// router.get('/api_get_project_details', api_projects.getProjectDetails);
+// router.get('/api_get_all_proj_bidded_by_user', api_projects.getAllProjBiddedByUser);
+// router.get('/api_get_all_proj_published_by_user', api_projects.getAllProjPublishedByUser);
+// router.put('/api_add_project', api_projects.postProject);
+// router.post('/api_hire_bid', api_projects.hireBid);
 
-router.get('/api_get_all_bids_on_proj', api_bids.getAllBidsOnProject);
-router.get('/api_get_ave_bid_price_on_proj', api_bids.getAveBidPriceOnProject);
-router.put('/api_add_bid_on_proj', api_bids.addBidOnProject);
+// router.get('/api_get_all_bids_on_proj', api_bids.getAllBidsOnProject);
+// router.get('/api_get_ave_bid_price_on_proj', api_bids.getAveBidPriceOnProject);
+// router.put('/api_add_bid_on_proj', api_bids.addBidOnProject);
 
 // Services on MongoDB
 router.get('/users/:id', api_m_users.getUser);
@@ -39,6 +39,7 @@ router.get('/users/:id/profile', api_m_users.getProfile);
 router.post('/users', api_m_users.createUser);
 router.post('/users/validation', api_m_users.validateUser);
 router.put('/users/:id', api_m_users.updateUser);
+router.put('/users/:id/avatar', api_m_users.updateAvatar);
 
 router.get('/projects', api_m_projects.searchProjects);
 router.get('/projects/:id', api_m_projects.getProjectDetails);
